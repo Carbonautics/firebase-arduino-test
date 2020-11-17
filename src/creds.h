499e7ba48078bea97c81a0b8724c42be516e9596
Pratik Nadgouda <nadgoudapratik@gmail.com>
2020-11-17 21:00:47 +0530

    Create creds.h

---
+++ src/creds.h
@@ -0,0 +1,4 @@
+#define secret_SSID "Fortress2-5Ghz"
+#define secret_pass "Carbon@20001027"
+#define FB_HOST "https://carb-test-13293.firebaseio.com/"
+#define FB_AUTH "50SCZbfafwaZpBVyYlqc4NJ5NA90Re6G0EB0sEyj"
